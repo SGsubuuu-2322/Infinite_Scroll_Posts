934c39ffd6b857bb491c0fec491d6aab61a9f5a2
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-24 22:16:34 +0530

    Added the scripts for the fetching and showing the post from API...

--- script.js
+++ script.js
@@ -9,4 +9,30 @@ async function getPosts() {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
   );
+
+  const data = res.json();
+
+  return data;
 }
+
+async function showPosts() {
+  const posts = await getPosts();
+
+  posts.forEach((post) => {
+    const postEl = document.createElement("div");
+    postEl.classList.add("post");
+    postEl.innerHTML = `
+    <div class="number">${post.id}</div>
+    <div class="post-info">
+      <h2 class="post-title">${post.title}</h2>
+      <p class="post-body">
+        ${post.body}
+      </p>
+    </div>
+    `;
+
+    post_container.appendChild(postEl);
+  });
+}
+
+showPosts();
